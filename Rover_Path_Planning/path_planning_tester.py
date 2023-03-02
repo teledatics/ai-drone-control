5751984bcfeabbc7b72ff6f8cc3aa2b5d1c1598c
mrjextreme6 <mrjextreme6@gmail.com>
2023-03-02 13:21:04 -0600

    Pixel to GPS coordinate conversion Python class (#1)
    -Created Rover_Path_Planning directory
    -Created path_planning module Python template
    -Added class to path_planning module for coverting pixel coordinates in drone aerial image to GPS coordinates
    
    ---------
    
    Co-authored-by: Justin Carrel <mrjextreme6>

---
+++ Rover_Path_Planning/path_planning_tester.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""! @brief Exercises path_planning module classes"""
+
+from path_planning import *
+
+
+def main():
+
+    image_width = 3840
+    image_height = 2160
+    pixelCoord = [image_width/4, image_height/4]
+    droneCoord = [-117.1666879, 33.03300795]
+    altitude = 394.028884
+    droneFOV = 59
+    compassHeading = 183.8
+
+    gpsCoordinate = ImageToGPSConverter.getPixelGPSCoordinate(
+        pixelCoord, image_width, image_height, droneCoord, altitude, droneFOV, compassHeading)
+    for idx in gpsCoordinate:
+        print(idx)
+
+
+if __name__ == "__main__":
+    main()
