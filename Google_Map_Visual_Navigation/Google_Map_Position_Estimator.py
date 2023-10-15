dcfc18dea96e0092c03a8f0c099c2be00b5f4a46
Justin Carrel <mrjextreme6@gmail.com>
2023-10-15 16:28:24 -0700

    -Started work on correlation filter for global localization step

--- Google_Map_Visual_Navigation/Google_Map_Position_Estimator.py
+++ Google_Map_Visual_Navigation/Google_Map_Position_Estimator.py
@@ -8,7 +8,7 @@
 #
 # @section description_Google_Map_Position_Estimator Description
 # Defines classes which encapsulate methodology of GPS position estimation from drone aerial images and onbaord Google Map data.
-# - FOO
+# - PositionEstimator
 #
 # @section libraries_Google_Map_Position_Estimator Libraries/Modules
 # - OpenCV (https://docs.opencv.org/4.x/index.html)
@@ -34,3 +34,70 @@
 # - https://browse.arxiv.org/pdf/1703.10125.pdf
 #
 # Copyright © 2023 Teledatics. All rights reserved.
+
+import numpy as np
+import cv2
+
+class PositionEstimator:
+    """
+    Position estimator class.
+
+    Defines class which encapsulates methods for providing continuous GPS positon estimations via pattern recognition with
+    drone telemetry, video feed, and an onboard Google Map reference image.
+    """
+    
+    ##################
+    # Public Methods #
+    ##################
+    
+    """
+    Constructor
+    
+    @name __init__
+    """
+    def __init__(self, googleMapImg):
+        self.googleMapImg = googleMapImg
+    
+    @classmethod
+    def initGlobalLocalization(self, droneImg):
+        # TODO: normalize image frame based on drone telemetry
+        f = cv2.cvtColor(droneImg, cv2.COLOR_BGR2GRAY)
+        f = f.astype(np.float32)
+        f = self.__pre_process(f)
+        F = np.fft.fft2(f)
+        
+        h = cv2.cvtColor(self.googleMapImg, cv2.COLOR_BGR2GRAY)
+        h = h.astype(np.float32)
+        h = self.__pre_process(h)
+        H_compConj = np.conjugate(np.fft.fft2(h)) # H*
+        
+        # NEXT: Need to create kernel based operation correlation method
+        
+        return
+    
+    ###################
+    # Private Methods #
+    ###################
+    
+    @classmethod
+    # pre-processing the image...
+    def __pre_process(self, img):
+        # get the size of the img...
+        height, width = img.shape
+        img = np.log(img + 1)
+        img = (img - np.mean(img)) / (np.std(img) + 1e-5)
+        # use the hanning window...
+        window = self.__window_func_2d(height, width)
+        img = img * window
+
+        return img
+    
+    @classmethod
+    def __window_func_2d(self, height, width):
+        win_col = np.hanning(width)
+        win_row = np.hanning(height)
+        mask_col, mask_row = np.meshgrid(win_col, win_row)
+
+        win = mask_col * mask_row
+
+        return win
---
+++ Google_Map_Visual_Navigation/mosse_filter/mosse.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python3
+"""@brief Defines Mosse object tracking filter"""
+
+##
+# @file mosse.py
+#
+# @brief Defines Mosse object tracking filter
+#
+# @section description_mosse Description
+# Defines classes which encapsulate Mosse object tracking filter algorithm.
+# - mosse
+#
+# @section libraries_mosse Libraries/Modules
+# - Numpy (https://numpy.org/)
+#   - Access to numerical computing tools for matrix calculations
+# - OpenCV (https://docs.opencv.org/4.x/index.html)
+#   - Access to computer vision and image processing algorithms
+# - os (https://docs.python.org/3/library/os.html)
+#   - Access to operating system interfaces 
+#
+# @section author_mosse Author(s)
+# - Originally authored by TianhongDai 05/28/2018
+# - Adapted by Justin Carrel on 10/15/2023
+#
+# @section references_mosse References
+# - https://github.com/TianhongDai/mosse-object-tracking
+# - https://www.cs.colostate.edu/~draper/papers/bolme_cvpr10.pdf
+#
+# Copyright © 2023 Teledatics. All rights reserved.
+
+import numpy as np
+import cv2
+import os
+from utils import linear_mapping, pre_process, random_warp
+
+class mosse:
+    def __init__(self, lr=0.125, sigma=100.0, num_pretrain=128, rotate=True):
+        self.lr = lr # learning rate
+        self.sigma = sigma # the sigma
+        self.num_pretrain = num_pretrain # number of pretraining cycles
+        self.rotate = rotate # rotate image during pretraining
+    
+    # start to do the object tracking...
+    def start_tracking(self):
+        # get the image of the first frame... (read as gray scale image...)
+        init_img = cv2.imread(self.frame_lists[0])
+        init_frame = cv2.cvtColor(init_img, cv2.COLOR_BGR2GRAY)
+        init_frame = init_frame.astype(np.float32)
+        # get the init ground truth.. [x, y, width, height]
+        init_gt = cv2.selectROI('demo', init_img, False, False)
+        init_gt = np.array(init_gt).astype(np.int64)
+        # start to draw the gaussian response...
+        response_map = self._get_gauss_response(init_frame, init_gt)
+        # start to create the training set ...
+        # get the goal..
+        g = response_map[init_gt[1]:init_gt[1]+init_gt[3], init_gt[0]:init_gt[0]+init_gt[2]]
+        fi = init_frame[init_gt[1]:init_gt[1]+init_gt[3], init_gt[0]:init_gt[0]+init_gt[2]]
+        G = np.fft.fft2(g)
+        # start to do the pre-training...
+        Ai, Bi = self._pre_training(fi, G)
+        # start the tracking...
+        for idx in range(len(self.frame_lists)):
+            current_frame = cv2.imread(self.frame_lists[idx])
+            frame_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+            frame_gray = frame_gray.astype(np.float32)
+            if idx == 0:
+                Ai = self.args.lr * Ai
+                Bi = self.args.lr * Bi
+                pos = init_gt.copy()
+                clip_pos = np.array([pos[0], pos[1], pos[0]+pos[2], pos[1]+pos[3]]).astype(np.int64)
+            else:
+                Hi = Ai / Bi
+                fi = frame_gray[clip_pos[1]:clip_pos[3], clip_pos[0]:clip_pos[2]]
+                fi = pre_process(cv2.resize(fi, (init_gt[2], init_gt[3])))
+                Gi = Hi * np.fft.fft2(fi)
+                gi = linear_mapping(np.fft.ifft2(Gi))
+                # find the max pos...
+                max_value = np.max(gi)
+                max_pos = np.where(gi == max_value)
+                dy = int(np.mean(max_pos[0]) - gi.shape[0] / 2)
+                dx = int(np.mean(max_pos[1]) - gi.shape[1] / 2)
+                
+                # update the position...
+                pos[0] = pos[0] + dx
+                pos[1] = pos[1] + dy
+
+                # trying to get the clipped position [xmin, ymin, xmax, ymax]
+                clip_pos[0] = np.clip(pos[0], 0, current_frame.shape[1])
+                clip_pos[1] = np.clip(pos[1], 0, current_frame.shape[0])
+                clip_pos[2] = np.clip(pos[0]+pos[2], 0, current_frame.shape[1])
+                clip_pos[3] = np.clip(pos[1]+pos[3], 0, current_frame.shape[0])
+                clip_pos = clip_pos.astype(np.int64)
+
+                # get the current fi..
+                fi = frame_gray[clip_pos[1]:clip_pos[3], clip_pos[0]:clip_pos[2]]
+                fi = pre_process(cv2.resize(fi, (init_gt[2], init_gt[3])))
+                # online update...
+                Ai = self.args.lr * (G * np.conjugate(np.fft.fft2(fi))) + (1 - self.args.lr) * Ai
+                Bi = self.args.lr * (np.fft.fft2(fi) * np.conjugate(np.fft.fft2(fi))) + (1 - self.args.lr) * Bi
+            
+            # visualize the tracking process...
+            cv2.rectangle(current_frame, (pos[0], pos[1]), (pos[0]+pos[2], pos[1]+pos[3]), (255, 0, 0), 2)
+            cv2.imshow('demo', current_frame)
+            cv2.waitKey(100)
+
+
+    # pre train the filter on the first frame...
+    def _pre_training(self, init_frame, G):
+        height, width = G.shape
+        fi = cv2.resize(init_frame, (width, height))
+        # pre-process img..
+        fi = pre_process(fi)
+        Ai = G * np.conjugate(np.fft.fft2(fi))
+        Bi = np.fft.fft2(init_frame) * np.conjugate(np.fft.fft2(init_frame))
+        for _ in range(self.args.num_pretrain):
+            if self.args.rotate:
+                fi = pre_process(random_warp(init_frame))
+            else:
+                fi = pre_process(init_frame)
+            Ai = Ai + G * np.conjugate(np.fft.fft2(fi))
+            Bi = Bi + np.fft.fft2(fi) * np.conjugate(np.fft.fft2(fi))
+        
+        return Ai, Bi
+
+    # get the ground-truth gaussian reponse...
+    def _get_gauss_response(self, img, gt):
+        # get the shape of the image..
+        height, width = img.shape
+        # get the mesh grid...
+        xx, yy = np.meshgrid(np.arange(width), np.arange(height))
+        # get the center of the object...
+        center_x = gt[0] + 0.5 * gt[2]
+        center_y = gt[1] + 0.5 * gt[3]
+        # cal the distance...
+        dist = (np.square(xx - center_x) + np.square(yy - center_y)) / (2 * self.args.sigma)
+        # get the response map...
+        response = np.exp(-dist)
+        # normalize...
+        response = linear_mapping(response)
+        return response
+
+    # it will extract the image list 
+    def _get_img_lists(self, img_path):
+        frame_list = []
+        for frame in os.listdir(img_path):
+            if os.path.splitext(frame)[1] == '.jpg':
+                frame_list.append(os.path.join(img_path, frame)) 
+        return frame_list
+    
+    # it will get the first ground truth of the video..
+    def _get_init_ground_truth(self, img_path):
+        gt_path = os.path.join(img_path, 'groundtruth.txt')
+        with open(gt_path, 'r') as f:
+            # just read the first frame...
+            line = f.readline()
+            gt_pos = line.split(',')
+
+        return [float(element) for element in gt_pos]
+
